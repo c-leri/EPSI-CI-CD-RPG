6ebd858f4a9e039dd29eefe5701298b403f05728
Vadim Spac <shvadim92232@gmail.com>
2025-06-17 15:56:36 +0200

    some fix

--- front/src/lib/Api/CallApi/GetGame.ts
+++ front/src/lib/Api/CallApi/GetGame.ts
@@ -2,8 +2,10 @@ import type { ApiResponse,Game } from '$lib/types/api';
 import { API_CONFIG } from '$lib/Api/configapi/config';
 
 export const roomApi = {
-	async getGame(): Promise<ApiResponse<Game>> {
-		const response = await fetch(`${API_CONFIG.PUBLIC_BACKEND_URL}${API_CONFIG.ENDPOINTS.Game.gameGet}`.replace('{id}', '1'));
+	async getGame(id: string): Promise<ApiResponse<Game>> {
+		console.log(id)
+		console.log(`${API_CONFIG.BACKEND_URL}${API_CONFIG.ENDPOINTS.Game.gameGet.replace('{id}', id)}`)
+		const response = await fetch(`${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.gameGet.replace('{id}', id)}`);
 		const data: ApiResponse<Game> = await response.json();
 
 		if (!response.ok) {
