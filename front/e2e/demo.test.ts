6eaa73eb530624e1a58bcffb4accd780d8e2fa05
Pradityo Adi WICAKSONO <pradityo.wicaksono@yahoo.com>
2025-06-18 10:53:21 +0200

    add test on load game's modal

--- front/e2e/demo.test.ts
+++ front/e2e/demo.test.ts
@@ -4,3 +4,18 @@ test('home page has expected h1', async ({ page }) => {
 	await page.goto('/');
 	await expect(page.locator('h1').first()).toBeVisible();
 });
+
+test('Click "Charger une sauvegarde" shows modal with correct title and 5 buttons', async ({ page }) => {
+	await page.goto('http://localhost:3000');
+
+
+	await page.getByRole('button', { name: /Charger une sauvegarde/i }).click();
+
+
+	await expect(page.getByText(/Choisissez votre numéro de sauvegarde/i)).toBeVisible();
+
+	const buttons = await page.locator('button').all();
+	expect(buttons).toHaveLength(5);
+});
+
+
