bc9b61f2260707c7551e18de04331537eac808b8
Vadim Spac <shvadim92232@gmail.com>
2025-06-18 11:37:24 +0200

    Add gitAll for backend

--- back/src/main/java/io/github/cleri/epsicicdrpg/back/service/GameService.java
+++ back/src/main/java/io/github/cleri/epsicicdrpg/back/service/GameService.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 import io.github.cleri.epsicicdrpg.back.model.Game;
 import io.github.cleri.epsicicdrpg.back.repository.GameRepository;
 import lombok.RequiredArgsConstructor;
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
@@ -17,6 +18,12 @@ public class GameService {
         
     }
 
+    public List<Game> getAllGame(){
+        List<Game> listGame = gameRepository.findAll();
+
+        return listGame;
+
+    }
 
     public Game getGameById(Long id) {
         return gameRepository.findById(id).orElse(null);
