4f2e87adba5680d275eeebbd89f6e8718dc9fb6b
Vadim Spac <shvadim92232@gmail.com>
2025-06-18 14:12:08 +0200

    get all games

--- front/src/lib/Api/CallApi/gameApi.ts
+++ front/src/lib/Api/CallApi/gameApi.ts
@@ -3,12 +3,15 @@ import { API_CONFIG } from '$lib/Api/configapi/config';
 
 export const gameApi = {
 	async createGame(): Promise<number> {
-		const response = await fetch(`${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.gameCreate}`, {
-			method: 'POST',
-			headers: {
-				'Content-Type': 'application/json'
+		const response = await fetch(
+			`${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.gameCreate}`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
 			}
-		});
+		);
 
 		if (!response.ok) {
 			throw new Error('Erreur lors de la création du jeu');
@@ -18,36 +21,50 @@ export const gameApi = {
 		return parseInt(gameId);
 	},
 
+	async getAllGames(): Promise<Game[]> {
+		const response = await fetch(
+			`${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.getAllGames}`
+		);
+
+		if (!response.ok) {
+			throw new Error('Erreur lors de la récupération des jeux');
+		}
+
+		const data: Game[] = await response.json();
+		console.log('All games data:', data);
+		return data;
+	},
+
 	async loadGame(id: number): Promise<ApiResponse<Game>> {
-		console.log("loadGame called with id:", id);
-		const response = await fetch(`${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.gameGet.replace('{id}', id.toString())}`);
+		console.log('loadGame called with id:', id);
+		const response = await fetch(
+			`${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.gameGet.replace('{id}', id.toString())}`
+		);
 
 		if (!response.ok) {
 			throw new Error('Jeu non trouvé');
 		}
 
 		const data: ApiResponse<Game> = await response.json();
-		console.log("Response data:", data);
+		console.log('Response data:', data);
 		return data;
 	},
 
-    async playNextRoom(id: number): Promise<ApiResponse<Game>> {
-        console.log("playNextRoom called with id:", id);
+	async playNextRoom(id: number): Promise<ApiResponse<Game>> {
+		console.log('playNextRoom called with id:', id);
 
-        const response = await fetch(
-            `${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.gameNextRoom.replace('{id}', id.toString())}`,
-            {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-            }
-        );
-		
+		const response = await fetch(
+			`${API_CONFIG.BACKEND_URL}/${API_CONFIG.ENDPOINTS.Game.gameNextRoom.replace('{id}', id.toString())}`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json'
+				}
+			}
+		);
 
 		const data: ApiResponse<Game> = await response.json();
-		console.log("Response data:", data);
+		console.log('Response data:', data);
 		return data;
-    }
-
-}
+	}
+};
